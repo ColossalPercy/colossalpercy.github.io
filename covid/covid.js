7288d1816014fbcb4a728c39ba21b092f28e8203
ColossalPercy <jackspercy@gmail.com>
2020-11-12 21:35:50 +0000

    add covid tracker

---
+++ covid/covid.js
@@ -0,0 +1,98 @@
+const utlaEndpoint = (
+    'https://api.coronavirus.data.gov.uk/v1/data?' +
+    'filters=areaType=utla' +
+    '&latestBy=cumCasesByPublishDate' +
+    '&structure={"areaName":"areaName","cases":"cumCasesByPublishDate","rate":"cumCasesByPublishDateRate"}'
+);
+
+const nationEndpoint = (
+    'https://api.coronavirus.data.gov.uk/v1/data?' +
+    'filters=areaType=nation' +
+    '&latestBy=cumCasesByPublishDate' +
+    '&structure={"areaName":"areaName","cases":"cumCasesByPublishDate","rate":"cumCasesByPublishDateRate"}'
+);
+
+const regions = [
+    'Belfast',
+    'Birmingham',
+    'Bradford',
+    'Bristol, City of',
+    'City of Edinburgh',
+    'Glasgow City',
+    'Leeds',
+    'Leicester',
+    'Essex',
+    'Manchester',
+    'Newcastle upon Tyne',
+    'Nottinghamshire'
+]
+
+let copyData = null;
+
+const getData = async () => {
+    var utlaResult = await fetch(utlaEndpoint, { "method": "GET" });
+    var nationResult = await fetch(nationEndpoint, { "method": "GET" });
+
+    document.querySelector('.area-date').innerHTML = utlaResult.headers.get('Last-Modified');
+    document.querySelector('.nation-date').innerHTML = nationResult.headers.get('Last-Modified');
+    
+    const utlaData = (await utlaResult.json()).data;
+    const nationData = (await nationResult.json()).data;
+
+    const data = {
+        northernIreland: nationData.find(d => d.areaName === 'Northern Ireland'),
+        scotland: nationData.find(d => d.areaName === 'Scotland'),
+        belfast: utlaData.find(d => d.areaName === 'Belfast'),
+        birmingham: utlaData.find(d => d.areaName === 'Birmingham'),
+        bradford: utlaData.find(d => d.areaName === 'Bradford'),
+        bristol: utlaData.find(d => d.areaName === 'Bradford'),
+        edinburgh: utlaData.find(d => d.areaName === 'City of Edinburgh'),
+        glasgow: utlaData.find(d => d.areaName === 'Glasgow City'),
+        leeds: utlaData.find(d => d.areaName === 'Leeds'),
+        leicester: utlaData.find(d => d.areaName === 'Leicester'),
+        essex: utlaData.find(d => d.areaName === 'Essex'),
+        manchester: utlaData.find(d => d.areaName === 'Manchester'),
+        newcastle: utlaData.find(d => d.areaName === 'Newcastle upon Tyne'),
+        nottinghamshire: utlaData.find(d => d.areaName === 'Nottinghamshire'),
+    }
+
+    const result = [
+        data.northernIreland.rate,
+        data.belfast.rate,
+        data.birmingham.rate,
+        data.bradford.rate,
+        data.bristol.rate,
+        data.edinburgh.rate,
+        data.glasgow.rate,
+        data.leeds.rate,
+        data.leicester.rate,
+        data.scotland.rate,
+        data.essex.rate,
+        data.manchester.rate,
+        data.newcastle.rate,
+        data.nottinghamshire.rate,
+        '',
+        data.northernIreland.cases,
+        data.belfast.cases,
+        data.birmingham.cases,
+        data.bradford.cases,
+        data.bristol.cases,
+        data.edinburgh.cases,
+        data.glasgow.cases,
+        data.leeds.cases,
+        data.leicester.cases,
+        data.essex.cases,
+        data.manchester.cases,
+        data.newcastle.cases,
+        data.nottinghamshire.cases,
+    ];
+
+    copyData = result.join('\t');
+    document.querySelector('button').disabled = false;
+}
+
+getData();
+
+copy = () => {
+  navigator.clipboard.writeText(copyData);
+}
